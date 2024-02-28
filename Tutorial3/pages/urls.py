e57cfd2010218f7e2088bc60910f9d48492c15f1
Daniel Velez <esovelez@gmail.com>
2024-02-28 13:52:58 -0500

    Parte B, D
    Rest of the code 1

--- Tutorial3/pages/urls.py
+++ Tutorial3/pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido, CartView, CartRemoveAllView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido, CartView, CartRemoveAllView, ImageViewFactory
+from .utils import ImageLocalStorage
 
 urlpatterns = [
     path("", HomePageView.as_view(), name = 'home'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name = 'cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name = 'cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name = 'cart_removeAll'),
-
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
 ]
--- Tutorial3/pages/views.py
+++ Tutorial3/pages/views.py
@@ -176,3 +176,17 @@ class CartRemoveAllView(View):
             del request.session['cart_product_data']
 
         return redirect('cart_index')
+    
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+    return ImageView
