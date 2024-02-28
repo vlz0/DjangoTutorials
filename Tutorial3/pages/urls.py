0e5ae486bdedc9010301f7dee7f62d56dcfabbac
Daniel Velez <esovelez@gmail.com>
2024-02-28 13:24:41 -0500

    Parte A
    A cart system

--- Tutorial3/pages/urls.py
+++ Tutorial3/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido, CartView, CartRemoveAllView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name = 'home'),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('products/valido', ProductoValido.as_view(), name = 'valido'),
     path('products/create', ProductCreateView.as_view(), name = 'form'),
     path('products/<str:id>', ProductShowView.as_view(), name = 'show'),
+    path('cart/', CartView.as_view(), name = 'cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name = 'cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name = 'cart_removeAll'),
+
 ]
